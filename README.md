15b133c4b159b100fc99ed5f531db64c7dad8f61
김선영 <tjsgur35@betree.co.kr>
2021-01-14 15:36:07 +0900

    filp 연습

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # gsap flip 연습해보자!
 
-- gsap flip
+###append.html
+- 브라우저 사이즈에 따라 요소 위치 변경
+
+###letter.html
+- 글자 애니메이션
--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,59 @@ import { gsap } from 'gsap';
 import { Flip } from 'gsap/Flip';
 
 window.script = (($) => {
-	const test = () => {
-		alert('테스트입니다');
+	gsap.registerPlugin(Flip);
+	const text = $('.container');
+
+	const eventLetter = () => {
+		const layouts = ['code1', 'code2', 'code3'];
+		let current = 0;
+		const state = Flip.getState('.letter', { props: 'color,backgroundColor', simple: true });
+		text.removeClass(layouts[current]);
+		current = (current + 1) % layouts.length;
+		text.addClass(layouts[current]);
+		Flip.from(state, {
+			absolute: true,
+			duration: 0.5,
+			stagger: 0.1,
+			ease: 'power1.inOut',
+			simple: true,
+			spin: current === 2,
+		});
+		gsap.delayedCall(current === 0 ? 3.5 : 1.5, event);
 	};
 
-	return {
-		test: test,
+	const wrapper = $('.wrapper');
+	const box = $('.wrapper .text');
+
+	const eventText = () => {
+		textAppend();
+		$(window).on('resize', () => {
+			textAppend();
+		});
 	};
+
+	const textAppend = () => {
+		let width = window.innerWidth;
+		let current = 0;
+		if (width > 1440) {
+			current = 0;
+		} else if (width > 1024) {
+			current = 1;
+		} else if (width > 768) {
+			current = 2;
+		}
+		if ($(`.section${current + 1} .text`).length > 0) return;
+
+		const state = Flip.getState(box);
+		$(`.section${current + 1}`).append(box);
+		Flip.from(state, { duration: 0.8, ease: 'power1.inOut', scale: true });
+	};
+
+	eventText();
+
+	if (text.length > 0) {
+		gsap.delayedCall(1, eventLetter);
+	}
+
+	return {};
 })($);
